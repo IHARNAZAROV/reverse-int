ca06ef11489dea081e3e8674fd1d59eea76d7bab
Igor Nazarov <grizley.tery@gmail.com>
2020-10-05 09:36:59 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+const {neq} = require("semver");
+
+module.exports = function reverse(n) {
+    let array = String(n).split('');
+    let newarray = [];
+    for (let i = String(n).length - 1; i >= 0; i -= 1)
+    {
+        newarray.push(array[i]);
+    }
+    return parseInt(newarray.join(''));
 }
